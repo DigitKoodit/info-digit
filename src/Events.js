2659ca8ff09e047e2abe97d3d0081bbd07290788
jwwallin <jussi.w.wallin@gmail.com>
2017-02-22 12:00:01 +0200

    Clear excess stuffs

--- src/Events.js
+++ src/Events.js
@@ -1,21 +1,14 @@
 import React, { Component } from 'react';
-import BigCalendar from 'react-big-calendar';
 import moment from 'moment';
 import './Events.css';
 
-BigCalendar.momentLocalizer(moment);
-
-require('react-big-calendar/lib/css/react-big-calendar.css')
 
 class Events extends Component {
 
     render() {
         return (
             <div>
-                <BigCalendar 
-                    style={{height:'420px'}}
-                    events={[]}
-                />
+
             </div>
         );
     }
