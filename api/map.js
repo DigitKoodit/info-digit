b4cf0fcf9c19fb2096e06fe3151e54a7b97fae20
Vaintti <antti.jk.vainikka@gmail.com>
2017-02-22 21:24:32 +0200

    Added empty api

---
+++ api/map.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var router = express.Router();
+
+// get all messages
+router.get('/map', function(req, res, next) {
+    // palauta res.send tai res.json tjms
+});
+
+module.exports = router;
