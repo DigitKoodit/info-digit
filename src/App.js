181124604e3ebbd3176fc50d8f761d72ef51dfd6
Vaintti <antti.jk.vainikka@gmail.com>
2017-06-01 19:52:16 +0300

    Created layout

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import logo from './digit_logo.svg';
 import './App.css';
 import Clock from './Clock';
 import Date from './Date';
@@ -8,23 +8,31 @@ import Events from './Events';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <div>
-            <Clock />
+      <div className="App vertical-flex-container" style={{border: 'solid thin'}}>
+        <div className="horizontal-flex-container" style={{flex: 1, border: 'solid thin'}}>
+          <div style={{flex: 1, border: 'solid thin', display: 'flex', justifyContent: 'left', paddingLeft: '1em'}}>
+            <div>
+              <img src={logo} alt="logo" style={{height: '100%'}}/>
+            </div>
           </div>
-          <div>
+          <div style={{flex: 1, display: 'flex', border: 'solid thin', justifyContent: 'center', alignContent: 'center', alignItems: 'center', fontSize: '3em'}}>
             <Date />
           </div>
-
-          <h2>Digit Noice sivu</h2>
+          <div style={{flex: 1, display: 'flex', border: 'solid thin', justifyContent: 'center', alignContent: 'center', alignItems: 'center', fontSize: '5em'}}>
+            <Clock />
+          </div>
         </div>
-        <div>
-          <Events />
+        <div style={{flex: 1, display: 'flex', border: 'solid thin', justifyContent: 'center', alignContent: 'center', alignItems: 'center', fontSize: '5em'}}>
+          INFORMAATIO SKRIINI
         </div>
-        
-        
+        <div className="horizontal-flex-container" style={{flex: 3, border: 'solid thin'}}>
+          <div style={{flex: 1, border: 'solid thin'}}></div>
+          <div style={{flex: 1, border: 'solid thin'}}><Events /></div>
+        </div>
+        <div className="horizontal-flex-container" style={{flex: 3, border: 'solid thin'}}>
+          <div style={{flex: 1, border: 'solid thin'}}></div>
+          <div style={{flex: 1, border: 'solid thin'}}></div>
+        </div>
       </div>
     );
   }
