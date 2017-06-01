d2b94af393edff066992968d510cdd3f750eea30
Vaintti <antti.jk.vainikka@gmail.com>
2017-06-01 20:38:28 +0300

    Added countdown component

--- src/App.js
+++ src/App.js
@@ -3,35 +3,37 @@ import logo from './digit_logo.svg';
 import './App.css';
 import Clock from './Clock';
 import Date from './Date';
-import Events from './Events';
+import Countdown from './Countdown';
+import moment from 'moment';
 
 class App extends Component {
   render() {
     return (
-      <div className="App vertical-flex-container" style={{border: 'solid thin'}}>
-        <div className="horizontal-flex-container" style={{flex: 1, border: 'solid thin'}}>
-          <div style={{flex: 1, border: 'solid thin', display: 'flex', justifyContent: 'left', paddingLeft: '1em'}}>
+      <div className="App vertical-flex-container" style={{backgroundColor: '#D4B16A'}}>
+        <div className="horizontal-flex-container" style={{flex: 1, borderBottom: 'solid thin', padding: '0.5em 1em', backgroundColor: '#FFE3AA'}}>
+          <div style={{flex: 1, display: 'flex', justifyContent: 'left'}}>
             <div>
               <img src={logo} alt="logo" style={{height: '100%'}}/>
             </div>
           </div>
-          <div style={{flex: 1, display: 'flex', border: 'solid thin', justifyContent: 'center', alignContent: 'center', alignItems: 'center', fontSize: '3em'}}>
+          <div style={{flex: 1, display: 'flex', justifyContent: 'center', alignContent: 'center', alignItems: 'center', fontSize: '3em'}}>
             <Date />
           </div>
-          <div style={{flex: 1, display: 'flex', border: 'solid thin', justifyContent: 'center', alignContent: 'center', alignItems: 'center', fontSize: '5em'}}>
+          <div style={{flex: 1, display: 'flex', justifyContent: 'flex-end', alignContent: 'center', alignItems: 'center', fontSize: '5em'}}>
             <Clock />
           </div>
         </div>
-        <div style={{flex: 1, display: 'flex', border: 'solid thin', justifyContent: 'center', alignContent: 'center', alignItems: 'center', fontSize: '5em'}}>
-          INFORMAATIO SKRIINI
+        <div style={{flex: 1, display: 'flex', flexDirection: 'column', borderBottom: 'solid thin', justifyContent: 'center', alignContent: 'center', alignItems: 'center', fontSize: '3em', backgroundColor: '#AA8439', color: 'white'}}>
+          <div>Wappuun on</div>
+          <Countdown date={moment('2018-5-1')} />
         </div>
-        <div className="horizontal-flex-container" style={{flex: 3, border: 'solid thin'}}>
-          <div style={{flex: 1, border: 'solid thin'}}></div>
-          <div style={{flex: 1, border: 'solid thin'}}><Events /></div>
+        <div className="horizontal-flex-container" style={{flex: 3}}>
+          <div style={{flex: 1}}></div>
+          <div style={{flex: 1}}></div>
         </div>
-        <div className="horizontal-flex-container" style={{flex: 3, border: 'solid thin'}}>
-          <div style={{flex: 1, border: 'solid thin'}}></div>
-          <div style={{flex: 1, border: 'solid thin'}}></div>
+        <div className="horizontal-flex-container" style={{flex: 3}}>
+          <div style={{flex: 1}}></div>
+          <div style={{flex: 1}}></div>
         </div>
       </div>
     );
---
+++ src/Countdown.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import moment from 'moment';
+
+class Countdown extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { now: new Date() };
+    }
+
+    componentDidMount() {
+        this.timer = setInterval(
+            () => this.tick(),
+            1000
+        );
+    }
+
+    componentWillMount() {
+        clearInterval(this.timer);
+    }
+
+    tick() {
+        this.setState({
+            now: new Date()
+        });
+    }
+
+    render() {
+        return (
+            <div>
+              {moment(this.props.date.valueOf()-this.state.now.valueOf()).format('M [kuukautta] D [päivää] h [tuntia] m [minuuttia] s [sekuntia]')}
+            </div>
+        );
+    }
+}
+
+export default Countdown;
